e8c81dcfdaf3d7e46c738de57aa1be680180ae68
Chase <123278965+chaszm@users.noreply.github.com>
2024-08-14 15:42:16 -0400

    profile,experience,projects, and contact components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import styles from "./App.module.css"
+import { NavBar } from "./components/NavBar/NavBar"
+import { Hero } from "./components/Hero/Hero"
+import { About } from "./components/About/About"
+import { Contact } from "./components/Contact/Contact"
+import { Experience } from "./components/Experience/Experience"
+import { Projects } from "./components/Projects/Projects"
 
 function App() {
-  return <div className={styles.App}> Hellow World </div>
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  )
 }
 
 export default App
