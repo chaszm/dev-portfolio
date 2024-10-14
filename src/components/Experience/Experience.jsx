586fd74c3d19a271b2600257b1e8c898621d67e5
Chase <123278965+chaszm@users.noreply.github.com>
2024-10-14 15:08:30 -0400

    added logos for skills, contact info

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,32 @@
 import styles from "./Experience.module.css"
 import skills from "../../data/skills.json"
-import history from "../../data/history.json"
+import languages from "../../data/languages.json"
 import { getImageUrl } from "../../utils"
+
+// divide skills into 2 categories, languages and tools
 
 export const Experience = () => {
   return (
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Skills</h2>
+      <div className={styles.sub}>
+        <h3>Languages</h3>
+        <h3>Frameworks/Tools</h3>
+      </div>
       <div className={styles.content}>
+        <div className={styles.skills}>
+          {languages.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            )
+          })}
+        </div>
+
         <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
